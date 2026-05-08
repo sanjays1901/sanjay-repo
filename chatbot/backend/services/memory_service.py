934478633721309f34d74bddd462f15b1513bf95
Sanjay Sharma <sanjay1901@gmail.com>
2026-05-08 15:51:50 +0530

    Updated redis client throwing error

--- chatbot/backend/services/memory_service.py
+++ chatbot/backend/services/memory_service.py
@@ -8,35 +8,57 @@ def build_session_key( tenant_id, session_id):
     return f"{tenant_id}:{session_id}"
 
 def get_history(tenant_id, session_id):
+
     if not session_id:
         return []
-    
-    session_key = build_session_key(tenant_id, session_id)
-    if not redis_client:
-        return []
-    # Geting data from Redis
-    history_json = redis_client.get(session_key)
-    if not history_json:
-        return []
-    history = json.loads(history_json)
 
-    return history[-6:]
+    try:
+
+        session_key = build_session_key(tenant_id, session_id)
+
+        history_json = redis_client.get(session_key)
+
+        if not history_json:
+            return []
+
+        history = json.loads(history_json)
+
+        return history[-6:]
+
+    except Exception as e:
+
+        print("Redis Error:", e)
+
+        return []
 
 
 def save_history(tenant_id, session_id, query, answer):
 
     if not session_id:
         return
-    
-    session_key = build_session_key(tenant_id, session_id)
-    if not redis_client:
-        return []
-    history_json = redis_client.get(session_key)
-    if history_json:
-        history = json.loads(history_json)
-    else:
-        history = []
 
-    history += [{"role": "user", "content": query}, {"role": "assistant", "content": answer}]
-    # Saving data to Redis
-    redis_client.setex(session_key, 3600, json.dumps(history))
+    try:
+
+        session_key = build_session_key(tenant_id, session_id)
+
+        history_json = redis_client.get(session_key)
+
+        if history_json:
+            history = json.loads(history_json)
+        else:
+            history = []
+
+        history += [
+            {"role": "user", "content": query},
+            {"role": "assistant", "content": answer}
+        ]
+
+        redis_client.setex(
+            session_key,
+            3600,
+            json.dumps(history)
+        )
+
+    except Exception as e:
+
+        print("Redis Error:", e)
